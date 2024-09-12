3fc4c93ae4931eaf3fb0e34505831ab0222e0ba9
Xavier <x.gavidia@pucp.edu.pe>
2024-09-11 20:01:11 -0500

    V1

---
+++ Lab3_20191802/src/main/java/com/example/lab3_20191802/Lab320191802Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20191802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320191802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab320191802Application.class, args);
+	}
+
+}
